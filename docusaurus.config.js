bea3c10b2a0e166ce464f48af14edf4023f87ddd
jinhojang6 <jinho5051@gmail.com>
2024-05-16 23:18:39 +0900

    feat: update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 // @ts-ignore
-require('dotenv').config()
+require("dotenv").config();
 // @ts-ignore
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Afaik.institute',
-  url: 'https://afaik.institute',
-  baseUrl: '/',
+  title: "Afaik.institute",
+  url: "https://afaik.institute",
+  baseUrl: "/",
 
   markdown: {
     mermaid: true,
@@ -17,24 +17,24 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      '@acid-info/logos-docusaurus-preset',
+      "@acid-info/logos-docusaurus-preset",
       /** @type {import('@acid-info/logos-docusaurus-preset').PluginOptions} */
       ({
-        businessUnit: 'Logos',
+        businessUnit: "Logos",
         theme: {
-          name: 'default',
+          name: "default",
           options: {
-            customCss: [require.resolve('./src/css/custom.scss')],
+            customCss: [require.resolve("./src/css/custom.scss")],
           },
         },
         docs: {
-          routeBasePath: '/',
+          routeBasePath: "/",
         },
       }),
     ],
@@ -42,60 +42,68 @@ const config = {
 
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'about',
-        routeBasePath: '/about',
-        path: 'about',
+        id: "about",
+        routeBasePath: "/about",
+        path: "about",
       },
     ],
   ],
 
   themeConfig:
-  /** @type {import('@acid-info/logos-docusaurus-preset').ThemeConfig} */
-      ({
-        colorMode: {
-          disableSwitch: true,
-          defaultMode: 'light',
-        },
-        navbar: {
-          hideOnScroll: true,
-          items: [
-            {
-              type: 'search',
-            },
-            {
-              label: 'Github',
-              href: 'https://github.com/acid-info',
-            },
-          ],
-        },
-        footer: {
-          copyright: 'afaik.institute @2023<br/>All Rights Reserved.',
-          links: [
-            {
-              items: [
-                {
-                  href: 'https://twitter.com/ac1d_info',
-                  label: 'Twitter',
-                },
-              ],
-            },
-            {
-              items: [
-                {
-                  href: 'https://jobs.status.im/',
-                  label: 'Work with us',
-                },
-                {
-                  href: '/terms',
-                  label: 'Terms & conditions',
-                },
-              ],
-            },
-          ],
-        }
-      }),
-}
+    /** @type {import('@acid-info/logos-docusaurus-preset').ThemeConfig} */
+    ({
+      colorMode: {
+        disableSwitch: true,
+        defaultMode: "light",
+      },
+      navbar: {
+        hideOnScroll: true,
+        items: [
+          {
+            type: "search",
+          },
+          {
+            label: "Github",
+            href: "https://github.com/acid-info",
+          },
+        ],
+      },
+      footer: {
+        copyright: "afaik.institute @2023<br/>All Rights Reserved.",
+        links: [
+          {
+            items: [
+              {
+                href: "https://twitter.com/ac1d_info",
+                label: "Twitter",
+              },
+            ],
+          },
+          {
+            items: [
+              {
+                href: "https://jobs.status.im/",
+                label: "Work with us",
+              },
+              {
+                href: "/terms",
+                label: "Terms & conditions",
+              },
+              {
+                href: "/privacy-policy",
+                label: "Privacy Policy",
+              },
+              {
+                href: "/security",
+                label: "Security",
+              },
+            ],
+          },
+        ],
+      },
+    }),
+};
 
-module.exports = config
+module.exports = config;
